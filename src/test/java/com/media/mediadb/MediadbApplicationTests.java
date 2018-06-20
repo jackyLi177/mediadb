60dd30e9b9614dd5e602290a0c5cc4d2c84a8727
1192964153@qq.com <1192964153@qq.com>
2018-06-20 11:47:55 +0800

    project initialization

---
+++ src/test/java/com/media/mediadb/MediadbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.media.mediadb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediadbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
